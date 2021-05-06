6f79fb7f0910c1f01794080784cc86d101dbcfc8
Marcelo Lopes <marcelo.utfpr@me.com>
2021-05-06 20:13:48 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/coamo/marcelo/atividade/AtividadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.coamo.marcelo.atividade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/coamo/marcelo/atividade/AtividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coamo.marcelo.atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
